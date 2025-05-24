296941bbdcebc5f09a285c612515ab13871372ab
Jack O'Connor <oconnor663@gmail.com>
2025-05-24 13:06:24 -0700

    separate init from wait, and remove the outer loop

--- src/lib.rs
+++ src/lib.rs
@@ -11,27 +11,35 @@ enum State {
     SomeoneIsWaiting,
 }
 
-fn sigchld_reader() -> io::Result<&'static UnixStream> {
-    // We don't want to put this self-pipe in the STATE mutex, because other threads need to be
-    // able to observe the state while one thread waits on this reader. We also don't want to make
-    // this a LazyLock, because if initialization fails for any reason (FD space exhausted?) we
-    // want to raise those errors and possibly retry. We could use a regular pipe instead of a Unix
-    // socket, but the standard library provides `set_nonblocking` for sockets, which saves us an
-    // unsafe libc call. The socket is bidirectional, but we'll only ever use this end as a reader.
-    static SIGCHLD_READER: OnceLock<UnixStream> = OnceLock::new();
+static SIGCHLD_READER: OnceLock<UnixStream> = OnceLock::new();
 
-    if let Some(reader) = SIGCHLD_READER.get() {
-        // The pipe has already been opened and registered.
-        return Ok(reader);
+/// Some thread must call `init` (successfully) before any thread calls [`wait`] or
+/// [`wait_timeout`].
+pub fn init() -> io::Result<()> {
+    // Double-check locking. The first check is the already-initialized fast path.
+    if SIGCHLD_READER.get().is_some() {
+        return Ok(());
     }
 
-    // We need to open and register the pipe.
+    // Take the state mutex, so that only one thread tries to initialize the pipe at a time. We
+    // don't want the mutex to own the reader, though, because many threads threads need to be able
+    // to observe the state while one thread blocks on the reader.
+    let _guard = STATE.lock().unwrap();
+
+    // Check again, because two threads could've raced to take the lock.
+    if SIGCHLD_READER.get().is_some() {
+        return Ok(());
+    }
+
+    // Open and register the pipe. We could use a regular pipe instead of a Unix socket, but the
+    // standard library provides `set_nonblocking` for sockets, which saves us an unsafe libc call.
+    // The socket is bidirectional, but we'll only ever write in one direction.
     let (reader, writer) = UnixStream::pair()?;
     reader.set_nonblocking(true)?;
     writer.set_nonblocking(true)?;
     signal_hook::low_level::pipe::register(signal_hook::consts::SIGCHLD, writer)?;
-    SIGCHLD_READER.set(reader).expect("one writer at a time");
-    Ok(SIGCHLD_READER.get().unwrap())
+    SIGCHLD_READER.set(reader).expect("only 1 thread gets here");
+    Ok(())
 }
 
 /// Block the current thread until any `SIGCHLD` signal arrives.
@@ -47,10 +55,10 @@ pub fn wait() -> io::Result<()> {
         drop(guard); // silence warnings
         return Ok(());
     }
-    // We're the thread that needs to (possibly initialize and) poll SIGCHLD_READER. Set the
-    // SomeoneIsWaiting state while we do this, and unlock the state mutex so that other threads
-    // can observe the state in the meantime. After doing this, we *must* unset the state before
-    // exiting. No short-circuiting with io errors.
+    // We're the thread that needs to poll SIGCHLD_READER. Set the SomeoneIsWaiting state while we
+    // do this, and unlock the state mutex so that other threads can observe the state in the
+    // meantime. After doing this, we *must* unset the state before exiting. No short-circuiting
+    // with io errors.
     *guard = State::SomeoneIsWaiting;
     drop(guard);
 
@@ -67,44 +75,34 @@ pub fn wait() -> io::Result<()> {
 
 // Within this function we can short-circuit. The caller manages state cleanup.
 fn wait_inner() -> io::Result<()> {
-    let mut reader = sigchld_reader()?;
-    loop {
-        // Wait until the pipe is readable.
-        let mut poll_fd = libc::pollfd {
-            fd: reader.as_raw_fd(),
-            events: libc::POLLIN,
-            revents: 0,
-        };
-        let error_code = unsafe {
-            libc::poll(
-                &mut poll_fd, // an "array" of one
-                1,            // the "array" length
-                -1,           // infinite timeout
-            )
-        };
-        if error_code < 0 {
-            return Err(io::Error::last_os_error());
-        }
-        // Read the pipe until EWOULDBLOCK. It could take more than one read, so we need an inner
-        // loop.
-        let mut did_read_anything = false;
-        let mut buf = [0u8; 1024];
-        loop {
-            match reader.read(&mut buf) {
-                Ok(0) => unreachable!("EOF"),
-                Ok(_) => did_read_anything = true,
-                Err(e) if e.kind() == ErrorKind::WouldBlock => break,
-                // EINTR should not be possible for a nonblocking read.
-                Err(e) => return Err(e),
-            }
-        }
-        // If we didn't actually read anything, poll must've woken us up spuriously. Continue the
-        // outer loop.
-        // TODO: timeouts
-        if !did_read_anything {
-            continue;
-        }
-        // We've been signaled.
-        return Ok(());
+    let mut reader = SIGCHLD_READER.get().expect("init must have been called");
+    // Wait until the pipe is readable.
+    let mut poll_fd = libc::pollfd {
+        fd: reader.as_raw_fd(),
+        events: libc::POLLIN,
+        revents: 0,
+    };
+    let error_code = unsafe {
+        libc::poll(
+            &mut poll_fd, // an "array" of one
+            1,            // the "array" length
+            -1,           // infinite timeout
+        )
+    };
+    if error_code < 0 {
+        return Err(io::Error::last_os_error());
     }
+    // Read the pipe until EWOULDBLOCK. This could take more than one read.
+    let mut buf = [0u8; 1024];
+    loop {
+        match reader.read(&mut buf) {
+            Ok(0) => unreachable!("this pipe should never close"),
+            Ok(_) => continue,
+            Err(e) if e.kind() == ErrorKind::WouldBlock => break,
+            // EINTR should not be possible for a nonblocking read.
+            Err(e) => return Err(e),
+        }
+    }
+    // Either we were signaled, or we woke up spuriously. We don't distinguish.
+    Ok(())
 }
